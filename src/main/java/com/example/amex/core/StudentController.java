7222789aa1249f1abf34692bf64d6f17037da30a
Jagadeeshk46 <jagadeeshk0546@gmail.com>
2024-05-08 19:02:48 +0530

    exception changes

--- src/main/java/com/example/amex/core/StudentController.java
+++ src/main/java/com/example/amex/core/StudentController.java
@@ -52,7 +52,7 @@ public class StudentController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found with " +id + name + studentClass);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
 		 
     }
 
